2a2aa1a2d03dd04000d6a531c1430a958634bf7a
someone <apple@x-MacBook-Pro.local>
2020-10-01 17:15:12 +0200

    quick

--- sys-account/generate.go
+++ sys-account/generate.go
@@ -5,7 +5,7 @@ package main
 // ============================================================================
 // GRPC & Protobuf
 //go:generate /usr/bin/env bash -c "echo 'Generating protobuf and grpc services golang'"
-//go:generate protoc -I./proto/v2 -I. --go_out=./server/rpc/v2/ --go-grpc_out=./server/rpc/v2 --cobra_out=./server/rpc/v2 --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --cobra_opt=paths=source_relative ./proto/v2/authn.proto ./proto/v2/users.proto
+//go:generate protoc -I./proto/v2 -I. --go_out=./service/go/rpc/v2/ --go-grpc_out=./service/go/rpc/v2 --cobra_out=./service/go/rpc/v2 --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --cobra_opt=paths=source_relative ./proto/v2/authn.proto ./proto/v2/users.proto
 //go:generate /usr/bin/env bash -c "echo 'Building example golang binaries (CLI and Server)'"
 //go:generate /usr/bin/env bash -c "mkdir -p bin-all/{cli,client}"
 //go:generate go build -v -o bin-all/cli ./example/cli/main.go
