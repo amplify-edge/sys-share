922f51f6e95692848462484473e33b8b9248b4c0
someone <apple@x-MacBook-Pro.local>
2020-10-08 15:48:07 +0200

    autocommit

---
+++ sys-account/example/server/go/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	// Import sys/Service ....
+)
+
+func main() {
+	fmt.Println(" Server  ")
+
+	// boo the Service
+}
---
+++ sys-core/example/cli/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	rpc "github.com/getcouragenow/sys-share/sys-core/service/go/rpc/v2"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "auth",
+	Short: "auth cli",
+}
+
+func main() {
+	rootCmd.AddCommand(rpc.AuthServiceClientCommand())
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("command failed: %v", err)
+	}
+
+	// Extend it here for local thing.
+	// TODO @gutterbacon: do this once Config is here.
+}
