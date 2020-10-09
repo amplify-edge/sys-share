cd9b2f655840df57c8386aab796ff43232b77dcd
gutterbacon <gutterbacon@protonmail.com>
2020-10-09 07:07:23 +0000

    fixes directory import path

--- sys-account/service/flu/lib/sys_share_sys_account_service.dart
+++ sys-account/service/flu/lib/sys_share_sys_account_service.dart
@@ -1,11 +1,11 @@
 library sys_share_sys_account_service;
 
-export 'api/v2/models.pb.dart';
-export 'api/v2/models.pbenum.dart';
-export 'api/v2/models.pbjson.dart';
+export 'rpc/v2/models.pb.dart';
+export 'rpc/v2/models.pbenum.dart';
+export 'rpc/v2/models.pbjson.dart';
 
-export 'api/v2/services.pb.dart';
-export 'api/v2/services.pbenum.dart';
-export 'api/v2/services.pbgrpc.dart';
-export 'api/v2/services.pbjson.dart';
+export 'rpc/v2/services.pb.dart';
+export 'rpc/v2/services.pbenum.dart';
+export 'rpc/v2/services.pbgrpc.dart';
+export 'rpc/v2/services.pbjson.dart';
 export 'view/view.dart';
