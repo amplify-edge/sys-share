25707fd4370b89df73b41e1eba797e19bf3ed6de
gutterbacon <gutterbacon@protonmail.com>
2020-10-09 07:05:57 +0000

    updated README directory structure, add flutter client example

---
+++ sys-account/example/client/flu/android/app/src/main/kotlin/org/getcouragenow/sys_share_account_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.getcouragenow.sys_share_account_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- sys-account/generate.go
+++ sys-account/generate.go
@@ -20,6 +20,6 @@ package main
 // ============================================================================
 // GRPC & Protobuf
 //go:generate /usr/bin/env bash -c "echo 'generating protobuf and grpc services for flutter/dart'"
-//go:generate protoc -I./proto/v2/ -I. --dart_out=grpc:./service/flu/lib/api/v2/ ./proto/v2/models.proto ./proto/v2/services.proto
+//go:generate protoc -I./proto/v2/ -I. --dart_out=grpc:./service/flu/lib/rpc/v2/ ./proto/v2/models.proto ./proto/v2/services.proto
 //go:generate /usr/bin/env bash -c "echo 'building flutter binary'"
 //go:generate make flu-web-build
