fc3cf7354162ddb0ea42336824a4e4181d2c6596
gutterbacon <gutterbacon@protonmail.com>
2020-10-17 19:02:10 +0000

    sys-core: added genrandombyteslice for configs

--- sys-core/service/config/config.go
+++ sys-core/service/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "io/ioutil"
+import (
+	"crypto/rand"
+	"io/ioutil"
+	"math/big"
+)
 
 // TODO @gutterbacon: use this as abstraction for per module configs.
 type Configer interface {
@@ -10,4 +14,17 @@ type Configer interface {
 
 func LoadFile(filepath string) ([]byte, error) {
 	return ioutil.ReadFile(filepath)
+}
+
+func GenRandomByteSlice(length int) ([]byte, error) {
+	const legalChars = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_"
+	rnd := make([]byte, length)
+	for i := 0; i < length; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(legalChars))))
+		if err != nil {
+			return nil, err
+		}
+		rnd = append(rnd, legalChars[num.Int64()])
+	}
+	return rnd, nil
 }
