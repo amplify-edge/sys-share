6dd874264d81e3989364fcb688e3d5b3d2521a4e
gutterbacon <gutterbacon@protonmail.com>
2020-10-23 21:14:27 +0700

    sys-core: removed old rpc and added newer ones

--- sys-core/service/flu/lib/sys_core.dart
+++ sys-core/service/flu/lib/sys_core.dart
@@ -9,6 +9,12 @@ export 'package:sys_core/device_utils.dart';
 export 'package:sys_core/pkg/i18n/languages.dart';
 export 'package:sys_core/pkg/widgets/gc_master_detail.dart';
 export 'package:sys_core/pkg/settings/core_settings.dart';
+export 'package:sys_core/rpc/v2/sys_core_models.pb.dart';
+export 'package:sys_core/rpc/v2/sys_core_models.pbjson.dart';
+export 'package:sys_core/rpc/v2/sys_core_models.pbenum.dart';
+export 'package:sys_core/rpc/v2/sys_core_services.pbgrpc.dart';
+export 'package:sys_core/rpc/v2/sys_core_services.pbjson.dart';
+export 'package:sys_core/rpc/v2/sys_core_services.pbenum.dart';
 
 coreInit() async {
   //final Directory dir = await getApplicationDocumentsDirectory();
