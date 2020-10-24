c40a5a310dc71a3df4959c758c152d0bee100fda
gutterbacon <gutterbacon@protonmail.com>
2020-10-24 19:13:20 +0700

    bench: support tls

--- sys-core/service/bench/bench.go
+++ sys-core/service/bench/bench.go
@@ -7,16 +7,32 @@ import (
 	"github.com/bojand/ghz/runner"
 )
 
-func RunBench(svcName, host, datapath, protopath string, total, concurrency uint) error {
-	report, err := runner.Run(
-		svcName,
-		host,
-		runner.WithProtoFile(protopath, []string{}),
-		runner.WithDataFromFile(datapath),
-		runner.WithInsecure(true),
-		runner.WithConcurrency(concurrency),
-		runner.WithTotalRequests(total),
-	)
+func RunBench(svcName, host, datapath, protopath string, total, concurrency uint, tlsEnabled bool, tlspath string) error {
+	var report *runner.Report
+	var err error
+	if tlsEnabled {
+		report, err = runner.Run(
+			svcName,
+			host,
+			runner.WithProtoFile(protopath, []string{}),
+			runner.WithDataFromFile(datapath),
+			//runner.WithInsecure(true),
+			runner.WithRootCertificate(tlspath),
+			runner.WithConcurrency(concurrency),
+			runner.WithTotalRequests(total),
+		)
+	} else {
+		report, err = runner.Run(
+			svcName,
+			host,
+			runner.WithProtoFile(protopath, []string{}),
+			runner.WithDataFromFile(datapath),
+			runner.WithInsecure(true),
+			runner.WithConcurrency(concurrency),
+			runner.WithTotalRequests(total),
+		)
+	}
+
 	if err != nil {
 		return err
 	}
