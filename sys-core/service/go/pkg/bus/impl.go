be5332e0c333c5882859b578c1ad8eed12684214
gutterbacon <gutterbacon@protonmail.com>
2020-11-02 05:56:08 +0000

    interceptor and bus edits

--- sys-core/service/go/pkg/bus/impl.go
+++ sys-core/service/go/pkg/bus/impl.go
@@ -2,6 +2,7 @@ package bus
 
 import (
 	"context"
+	"github.com/sirupsen/logrus"
 
 	"github.com/segmentio/encoding/json"
 
@@ -23,6 +24,7 @@ func (c *CoreBus) RegisterAction(name string, in ActionDispatcher) {
 }
 
 func (c *CoreBus) Broadcast(ctx context.Context, in *pkg.EventRequest) (*pkg.EventResponse, error) {
+	logrus.Warnf("%s is calling event for %s", in.Initiator, in.EventName)
 	if c.actions == nil {
 		return nil, Error{
 			Reason: errEmptyAction,
