286d4b2b4f99e55efb10186c466394aea365ca53
gutterbacon <gutterbacon@protonmail.com>
2020-11-22 23:40:47 +0700

    notification color fix

--- sys-core/service/flu/lib/pkg/widgets/notification.dart
+++ sys-core/service/flu/lib/pkg/widgets/notification.dart
@@ -15,14 +15,22 @@ void notify({
         controller: controller,
         style: flashStyle,
         boxShadows: [BoxShadow(blurRadius: 4)],
+        backgroundColor:
+            Theme.of(context).scaffoldBackgroundColor.withOpacity(0.7),
         barrierBlur: 3.0,
-        barrierColor: Colors.black38,
+        barrierColor:
+            Theme.of(context).scaffoldBackgroundColor.withOpacity(0.8),
         barrierDismissible: true,
         horizontalDismissDirection: HorizontalDismissDirection.horizontal,
         position: FlashPosition.top,
         child: FlashBar(
           leftBarIndicatorColor: error ? Colors.red[500] : Colors.green[500],
-          message: Text(message),
+          message: Text(
+            message,
+            style: TextStyle(
+              color: Theme.of(context).textTheme.subtitle2.color,
+            ),
+          ),
           primaryAction: IconButton(
             icon: Icon(Icons.close_rounded),
             onPressed: () => controller.dismiss(),
