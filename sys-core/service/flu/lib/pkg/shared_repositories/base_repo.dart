ce50288a3ed64c84770490335776e60c18a5b8ba
gutterbacon <gutterbacon@protonmail.com>
2020-12-16 09:12:33 +0000

    base repo modification

--- sys-core/service/flu/lib/pkg/shared_repositories/base_repo.dart
+++ sys-core/service/flu/lib/pkg/shared_repositories/base_repo.dart
@@ -18,6 +18,7 @@ class BaseRepo {
 
   static Future<GrpcWebClientChannel> grpcWebClientChannel() async {
     final hc = await getHostConfig();
+    print("HOST: ${hc.host}, PORT: ${hc.port}");
     return GrpcWebClientChannel.xhr(
         Uri(scheme: "https", host: hc.host, port: int.parse(hc.port)));
   }
