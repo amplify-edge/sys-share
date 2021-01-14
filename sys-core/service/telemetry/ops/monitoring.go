375626473cbfbffefec09bbd28c037847a9f464a
gutterbacon <gutterbacon@protonmail.com>
2021-01-14 07:33:05 +0000

    global system monitor using prometheus

--- sys-core/service/telemetry/ops/monitoring.go
+++ sys-core/service/telemetry/ops/monitoring.go
@@ -2,231 +2,134 @@ package ops
 
 import (
 	"context"
-	"github.com/VictoriaMetrics/metrics"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/sirupsen/logrus"
 	"time"
 
-	//"github.com/shirou/gopsutil/v3/disk"
-	//"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/net"
 )
 
-const (
-	OPS_METRICS_MEMORY  = "ops_memory_stats"
-	OPS_METRICS_CPU     = "ops_cpu_stats"
-	OPS_METRICS_DISK    = "ops_disk_usage_stats"
-	OPS_METRICS_NETWORK = "ops_network_stats"
-)
-
-type OpsMonitor struct {
-	ctx             context.Context
-	interval        time.Duration
-	netStats        map[string]net.IOCountersStat // interface-name as key
-	netStatsUpdated map[string]time.Time          // last updated time
-	systemStat      *models.SystemStat
-	nodeStat        *models.NodeStat
+type OpsSystemMonitor struct {
+	ctx              context.Context
+	logger           *logrus.Entry
+	interval         time.Duration
+	netStats         map[string]net.IOCountersStat // interface-name as key
+	netStatsUpdated  map[string]time.Time          // last updated time
+	systemStat       *SystemStat
+	metricsDiskPath  string
+	opsSystemMetrics *OpsSystemMetrics
 }
 
-type OpsMetricsSet struct {
-	MemoryStats  *metrics.Gauge
-	CpuStats     *metrics.Gauge
-	DiskStats    *metrics.Gauge
-	NetworkStats *metrics.Gauge
-}
-
-func NewOpsMetricsSet() *OpsMetricsSet {
-	return &OpsMetricsSet{
-		MemoryStats:  nil,
-		CpuStats:     nil,
-		DiskStats:    nil,
-		NetworkStats: nil,
+// NewOpsSystemMonitor is the constructor for OpsSystemMonitor
+// also registers system metrics and runtime metrics
+func NewOpsSystemMonitor(ctx context.Context, interval time.Duration, metricsDiskPath string) *OpsSystemMonitor {
+	opsSystemMetrics := NewOpsSystemMetrics()
+	opsSystemMetrics.RegisterMetrics()
+	if metricsDiskPath == "" {
+		metricsDiskPath = "/"
+	}
+	return &OpsSystemMonitor{
+		ctx:              ctx,
+		interval:         interval,
+		netStats:         make(map[string]net.IOCountersStat),
+		netStatsUpdated:  make(map[string]time.Time),
+		systemStat:       &SystemStat{},
+		metricsDiskPath:  metricsDiskPath,
+		opsSystemMetrics: opsSystemMetrics,
 	}
 }
 
-type OpsRuntimeSet struct {
-	
-}
-
-/*
-var (
-	memStatGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "system_mem_stat",
-		Help: "System mem stats",
-	}, []string{"type"})
-	cpuStatGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "system_cpu_stat",
-		Help: "System cpu stats",
-	}, []string{"type"})
-	diskUsageStatGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "system_disk_usage_stat",
-		Help: "System disk usage stats",
-	}, []string{"type"})
-	netStatCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "net_stat",
-		Help: "Network stat",
-	}, []string{"type", "interface"})
-)
-
-func init() {
-	prometheus.MustRegister(memStatGauge)
-	prometheus.MustRegister(cpuStatGauge)
-	prometheus.MustRegister(diskUsageStatGauge)
-	prometheus.MustRegister(netStatCounter)
-}
-
-// SystemCollector collects the system stat
-type SystemCollector struct {
-	ctx             context.Context
-	interval        time.Duration
-	storage         string
-	repository      state.Repository              // data will be putted to this
-	path            string                        // repository key
-	netStats        map[string]net.IOCountersStat // interface-name as key
-	netStatsUpdated map[string]time.Time          // last updated time
-	systemStat      *models.SystemStat
-	nodeStat        *models.NodeStat
-	// used for mock
-	MemoryStatGetter    MemoryStatGetter
-	CPUStatGetter       CPUStatGetter
-	DiskUsageStatGetter DiskUsageStatGetter
-	NetStatGetter       NetStatGetter
-}
-
-// NewSystemCollector creates a new system stat collector
-func NewSystemCollector(
-	ctx context.Context,
-	interval time.Duration,
-	storage string,
-	repository state.Repository,
-	path string,
-	node models.ActiveNode,
-) *SystemCollector {
-	r := &SystemCollector{
-		interval:        interval,
-		storage:         fileutil.GetExistPath(storage),
-		repository:      repository,
-		path:            path,
-		netStats:        make(map[string]net.IOCountersStat),
-		netStatsUpdated: make(map[string]time.Time),
-		systemStat:      &models.SystemStat{},
-		nodeStat: &models.NodeStat{
-			Node: node,
-		},
-		ctx:                 ctx,
-		MemoryStatGetter:    mem.VirtualMemory,
-		CPUStatGetter:       GetCPUStat,
-		DiskUsageStatGetter: disk.UsageWithContext,
-		NetStatGetter:       GetNetStat,
-	}
-	return r
-}
-
-// Run starts a background goroutine that collects the monitoring stat
-func (r *SystemCollector) Run() {
-	ticker := time.NewTicker(r.interval)
+func (o *OpsSystemMonitor) Run() {
+	ticker := time.NewTicker(o.interval)
 	defer ticker.Stop()
-	// collect system status
-	r.collect()
-
+	o.collectMetrics()
 	for {
 		select {
 		case <-ticker.C:
-			// collect system status
-			r.collect()
-		case <-r.ctx.Done():
+			o.collectMetrics()
+		case <-o.ctx.Done():
 			return
 		}
 	}
 }
 
-// collect collects the monitoring stat
-func (r *SystemCollector) collect() {
-	var err error
-	r.systemStat.CPUs = GetCPUs()
+func (o *OpsSystemMonitor) collectMetrics() {
+	cpuStat, err := cpu.TimesWithContext(o.ctx, false)
+	if err != nil {
+		o.logger.Errorf("getting cpu time stats error: %v", err)
+	}
+	o.systemStat.CPUTime = &cpuStat[0]
+	memstat, err := mem.VirtualMemoryWithContext(o.ctx)
+	if err != nil {
+		o.logger.Errorf("getting memory stats error: %v", err)
+	}
+	o.systemStat.MemoryStats = memstat
+	dustat, err := disk.UsageWithContext(o.ctx, o.metricsDiskPath)
+	if err != nil {
+		o.logger.Errorf("getting disk usage error: %v", err)
+	}
+	o.systemStat.DiskStats = dustat
+	netstats, err := net.IOCountersWithContext(o.ctx, true)
+	for _, ns := range netstats {
+		o.netStats[ns.Name] = ns
+		o.netStatsUpdated[ns.Name] = time.Now()
+	}
+	o.updatePrometheusStats()
+}
 
-	if r.systemStat.MemoryStat, err = r.MemoryStatGetter(); err != nil {
-		log.Error("get memory stat", logger.Error(err))
+func (o *OpsSystemMonitor) updatePrometheusStats() {
+	// CPU
+	cpuGauge := o.opsSystemMetrics.CpuStats
+	cpuStat := o.systemStat.CPUTime
+	if cpuGauge != nil && cpuStat != nil {
+		cpuGauge.WithLabelValues("total").Set(cpuStat.Total())
+		cpuGauge.WithLabelValues("user").Set(cpuStat.User)
+		cpuGauge.WithLabelValues("system").Set(cpuStat.System)
+		cpuGauge.WithLabelValues("idle").Set(cpuStat.Idle)
+		cpuGauge.WithLabelValues("nice").Set(cpuStat.Idle)
+		cpuGauge.WithLabelValues("soft_irq").Set(cpuStat.Softirq)
+		cpuGauge.WithLabelValues("irq").Set(cpuStat.Irq)
+		cpuGauge.WithLabelValues("iowait").Set(cpuStat.Iowait)
+		cpuGauge.WithLabelValues("steal").Set(cpuStat.Steal)
 	}
-	if r.systemStat.CPUStat, err = r.CPUStatGetter(); err != nil {
-		log.Error("get cpu stat", logger.Error(err))
+
+	// Memory
+	memGauge := o.opsSystemMetrics.MemoryStats
+	memstat := o.systemStat.MemoryStats
+	if memGauge != nil && memstat != nil {
+		memGauge.WithLabelValues("total").Set(float64(memstat.Total))
+		memGauge.WithLabelValues("free").Set(float64(memstat.Free))
+		memGauge.WithLabelValues("used").Set(float64(memstat.Used))
+		memGauge.WithLabelValues("used_percent").Set(float64(memstat.UsedPercent))
 	}
-	if r.systemStat.DiskUsageStat, err = r.DiskUsageStatGetter(r.ctx, r.storage); err != nil {
-		log.Error("get disk usage stat", logger.Error(err))
+
+	// Disk Usage
+	diskGauge := o.opsSystemMetrics.DiskStats
+	diskStat := o.systemStat.DiskStats
+	if diskGauge != nil && diskStat != nil {
+		diskGauge.WithLabelValues("total").Set(float64(diskStat.Total))
+		diskGauge.WithLabelValues("used").Set(float64(diskStat.Used))
+		diskGauge.WithLabelValues("free").Set(float64(diskStat.Free))
+		diskGauge.WithLabelValues("used_percent").Set(float64(diskStat.UsedPercent))
 	}
-	if stats, err := r.NetStatGetter(r.ctx); err != nil {
-		log.Error("get net stat", logger.Error(err))
-	} else {
-		for _, stat := range stats {
-			r.netStats[stat.Name] = stat
-			r.netStatsUpdated[stat.Name] = time.Now()
+
+	// Network
+	netGauge := o.opsSystemMetrics.NetworkStats
+	for _, ns := range o.netStats {
+		lastStat, ok := o.netStats[ns.Name]
+		if ok && time.Since(o.netStatsUpdated[ns.Name]) <= 2*o.interval {
+			netGauge.WithLabelValues("bytesSent", ns.Name).Add(float64(ns.BytesSent - lastStat.BytesSent))
+			netGauge.WithLabelValues("bytesRecv", ns.Name).Add(float64(ns.BytesRecv - lastStat.BytesRecv))
+			netGauge.WithLabelValues("packetsSent", ns.Name).Add(float64(ns.PacketsSent - lastStat.PacketsSent))
+			netGauge.WithLabelValues("packetsRecv", ns.Name).Add(float64(ns.PacketsRecv - lastStat.PacketsRecv))
+			netGauge.WithLabelValues("errin", ns.Name).Add(float64(ns.Errin - lastStat.Errin))
+			netGauge.WithLabelValues("errout", ns.Name).Add(float64(ns.Errout - lastStat.Errout))
+			netGauge.WithLabelValues("dropin", ns.Name).Add(float64(ns.Dropin - lastStat.Dropin))
+			netGauge.WithLabelValues("dropout", ns.Name).Add(float64(ns.Dropout - lastStat.Dropout))
 		}
-	}
-
-	r.nodeStat.System = *r.systemStat
-
-	r.logMemStat()
-	r.logDiskUsageStat()
-	r.logCPUStat()
-	r.logNetStat()
-
-	if err := r.repository.Put(r.ctx, r.path, encoding.JSONMarshal(r.nodeStat)); err != nil {
-		log.Error("report stat error", logger.String("path", r.path))
+		o.netStats[ns.Name] = ns
+		o.netStatsUpdated[ns.Name] = time.Now()
 	}
 }
-
-func (r *SystemCollector) logMemStat() {
-	if r.systemStat.MemoryStat != nil {
-		memStat := r.systemStat.MemoryStat
-		memStatGauge.WithLabelValues("total").Set(float64(memStat.Total))
-		memStatGauge.WithLabelValues("used").Set(float64(memStat.Used))
-		memStatGauge.WithLabelValues("used_percent").Set(memStat.UsedPercent)
-	}
-}
-
-func (r *SystemCollector) logCPUStat() {
-	if r.systemStat.CPUStat != nil {
-		cpuStat := r.systemStat.CPUStat
-		cpuStatGauge.WithLabelValues("idle").Set(cpuStat.Idle)
-		cpuStatGauge.WithLabelValues("nice").Set(cpuStat.Nice)
-		cpuStatGauge.WithLabelValues("system").Set(cpuStat.System)
-		cpuStatGauge.WithLabelValues("user").Set(cpuStat.User)
-		cpuStatGauge.WithLabelValues("irq").Set(cpuStat.Irq)
-		cpuStatGauge.WithLabelValues("steal").Set(cpuStat.Steal)
-		cpuStatGauge.WithLabelValues("softirq").Set(cpuStat.Softirq)
-		cpuStatGauge.WithLabelValues("iowait").Set(cpuStat.Iowait)
-	}
-}
-
-func (r *SystemCollector) logDiskUsageStat() {
-	if r.systemStat.DiskUsageStat != nil {
-		stat := r.systemStat.DiskUsageStat
-		// usage
-		diskUsageStatGauge.WithLabelValues("total").Set(float64(stat.Total))
-		diskUsageStatGauge.WithLabelValues("used").Set(float64(stat.Used))
-		diskUsageStatGauge.WithLabelValues("free").Set(float64(stat.Free))
-		diskUsageStatGauge.WithLabelValues("used_percent").Set(stat.UsedPercent)
-		// inode
-		diskUsageStatGauge.WithLabelValues("inodesFree").Set(float64(stat.InodesFree))
-		diskUsageStatGauge.WithLabelValues("inodesUsed").Set(float64(stat.InodesUsed))
-		diskUsageStatGauge.WithLabelValues("inodesTotal").Set(float64(stat.InodesTotal))
-		diskUsageStatGauge.WithLabelValues("inodesUsedPercent").Set(stat.InodesUsedPercent)
-	}
-}
-func (r *SystemCollector) logNetStat() {
-	for _, stat := range r.netStats {
-		lastStat, ok := r.netStats[stat.Name]
-		// check time interval
-		if ok && time.Since(r.netStatsUpdated[stat.Name]) <= 2*r.interval {
-			netStatCounter.WithLabelValues("bytesSent", stat.Name).Add(float64(stat.BytesSent - lastStat.BytesSent))
-			netStatCounter.WithLabelValues("bytesRecv", stat.Name).Add(float64(stat.BytesRecv - lastStat.BytesRecv))
-			netStatCounter.WithLabelValues("packetsSent", stat.Name).Add(float64(stat.PacketsSent - lastStat.PacketsSent))
-			netStatCounter.WithLabelValues("packetsRecv", stat.Name).Add(float64(stat.PacketsRecv - lastStat.PacketsRecv))
-			netStatCounter.WithLabelValues("errin", stat.Name).Add(float64(stat.Errin - lastStat.Errin))
-			netStatCounter.WithLabelValues("errout", stat.Name).Add(float64(stat.Errout - lastStat.Errout))
-			netStatCounter.WithLabelValues("dropin", stat.Name).Add(float64(stat.Dropin - lastStat.Dropin))
-			netStatCounter.WithLabelValues("dropout", stat.Name).Add(float64(stat.Dropout - lastStat.Dropout))
-		}
-		r.netStats[stat.Name] = stat
-		r.netStatsUpdated[stat.Name] = time.Now()
-	}
-}
-*/
