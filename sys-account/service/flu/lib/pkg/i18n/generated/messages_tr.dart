4b7b72544d59f372b9dae54ceb31cc42f1c0f85f
gutterbacon <gutterbacon@protonmail.com>
2021-01-24 23:32:49 +0700

    added Turkish translations

---
+++ sys-account/service/flu/lib/pkg/i18n/generated/messages_tr.dart
@@ -0,0 +1,41 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a tr locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'tr';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "byProceeding" : MessageLookupByLibrary.simpleMessage("Devam ederek Kullanım Koşullarımızı kabul ediyor ve Gizlilik Politikamızı okuduğunuzu onaylıyorsunuz."),
+    "email" : MessageLookupByLibrary.simpleMessage("Eposta"),
+    "emailHint" : MessageLookupByLibrary.simpleMessage("E-posta adresiniz"),
+    "forgotPassword" : MessageLookupByLibrary.simpleMessage("Parolanızı mı unuttunuz"),
+    "invalidEmail" : MessageLookupByLibrary.simpleMessage("Geçersiz e-posta"),
+    "invalidPassword" : MessageLookupByLibrary.simpleMessage("geçersiz şifre"),
+    "invalidVerificationToken" : MessageLookupByLibrary.simpleMessage("Geçersiz Doğrulama Jetonu"),
+    "password" : MessageLookupByLibrary.simpleMessage("Parola"),
+    "passwordHint" : MessageLookupByLibrary.simpleMessage("Şifreniz"),
+    "resetPassword" : MessageLookupByLibrary.simpleMessage("Şifreyi yenile"),
+    "signIn" : MessageLookupByLibrary.simpleMessage("Oturum aç"),
+    "signOut" : MessageLookupByLibrary.simpleMessage("Oturumu Kapat"),
+    "signUp" : MessageLookupByLibrary.simpleMessage("Kaydol"),
+    "submitNewPassword" : MessageLookupByLibrary.simpleMessage("Yeni Şifre Gönder"),
+    "verificationToken" : MessageLookupByLibrary.simpleMessage("Doğrulama Jetonu"),
+    "verifyAccount" : MessageLookupByLibrary.simpleMessage("Hesabı Doğrula")
+  };
+}
