72e96a2f747fa9cbbbb6ae4e1c4b47e528513f75
gutterbacon <gutterbacon@protonmail.com>
2021-02-23 11:46:37 +0000

    needs booty

--- Makefile
+++ Makefile
@@ -4,28 +4,24 @@ BOILERPLATE_FSPATH=$(SHARED_FSPATH)/boilerplate
 include $(BOILERPLATE_FSPATH)/help.mk
 include $(BOILERPLATE_FSPATH)/os.mk
 include $(BOILERPLATE_FSPATH)/gitr.mk
-include $(BOILERPLATE_FSPATH)/tool.mk
 include $(BOILERPLATE_FSPATH)/flu.mk
 include $(BOILERPLATE_FSPATH)/go.mk
 
-this-all: this-print this-dep this-build this-print-end
+all: print build print-end
  
 ## Print all settings
-this-print: 
+print:
 	@echo
 	@echo "-- SYS-SHARE: start --"
 	@echo
 	@echo
 
-this-print-end:
+print-end:
 	@echo
 	@echo "-- SYS-SHARE: end --"
 	@echo
 	@echo
 
-this-dep:
-	cd $(SHARED_FSPATH) && $(MAKE) this-dep
-
-this-build:
-	cd sys-core && $(MAKE) this-all
-	cd sys-account && $(MAKE) this-all
+build:
+	cd sys-core && $(MAKE) all
+	cd sys-account && $(MAKE) all
--- sys-account/generate.go
+++ sys-account/generate.go
@@ -5,13 +5,13 @@ package main
 // ============================================================================
 // GRPC & Protobuf
 //go:generate /usr/bin/env bash -c "echo 'Generating protobuf and grpc services golang'"
-//go:generate protoc -I./proto/v2/ -I. --go_out=./service/go/rpc/v2 --go_opt=paths=source_relative ./proto/v2/sys_account_models.proto
-//go:generate protoc -I./proto/v2/ -I. --go-grpc_out=./service/go/rpc/v2/ --cobra_out=./service/go/rpc/v2 --go-grpc_opt=paths=source_relative --cobra_opt=paths=source_relative ./proto/v2/sys_account_services.proto
-//go:generate protoc-go-inject-tag -input ./service/go/rpc/v2/sys_account_models.pb.go
+//go:generate booty protoc -I./proto/v2/ -I. --go_out=./service/go/rpc/v2 --go_opt=paths=source_relative ./proto/v2/sys_account_models.proto
+//go:generate booty protoc -I./proto/v2/ -I. --go-grpc_out=./service/go/rpc/v2/ --cobra_out=./service/go/rpc/v2 --go-grpc_opt=paths=source_relative --cobra_opt=paths=source_relative ./proto/v2/sys_account_services.proto
+//go:generate protoc-gen-inject-tag -input ./service/go/rpc/v2/sys_account_models.pb.go
 
 // ============================================================================
 // Flutter
 // ============================================================================
 // GRPC & Protobuf
 //go:generate /usr/bin/env bash -c "echo 'generating protobuf and grpc services for flutter/dart'"
-//go:generate protoc -I./proto/v2/ -I. --dart_out=grpc:./service/flu/lib/rpc/v2/ ./proto/v2/sys_account_models.proto ./proto/v2/sys_account_services.proto
+//go:generate booty protoc -I./proto/v2/ -I. --dart_out=grpc:./service/flu/lib/rpc/v2/ ./proto/v2/sys_account_models.proto ./proto/v2/sys_account_services.proto
